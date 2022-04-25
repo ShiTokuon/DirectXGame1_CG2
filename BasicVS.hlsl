b3e47c6839f95d129d5499e5daf77d17474acfff
ShiTokuon <k021g1160@g.neec.ac.jp>
2022-04-25 13:18:28 +0900

    02_02_プログラマシェーダ

--- BasicVS.hlsl
+++ BasicVS.hlsl
@@ -1,4 +1,11 @@
-float4 main( float4 pos : POSITION ) : SV_POSITION
+float4 main(float4 pos : POSITION) : SV_POSITION
 {
-	return pos;
+	float4 a;
+float4 b = { 1,1,1,1 };
+float4 pos2 = pos;
+const float PI = 3.14159;
+float angle = PI * 0.25;
+pos2.x = pos.x * cos(angle) - pos.y * sin(angle);
+pos2.y = pos.x * sin(angle) + pos.y * cos(angle);
+return pos2;
 }
--- main.cpp
+++ main.cpp
@@ -207,7 +207,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	XMFLOAT3 vertices[] = {
 		{ -0.5f, -0.5f, 0.0f }, // 左下
 		{ -0.5f, +0.5f, 0.0f }, // 左上
-		{ +0.5f, -0.5f, 0.0f }, // 右下
+		{ 0.0f, 0.0f, 0.0f }, // 右下
 	};
 	// 頂点データ全体のサイズ = 頂点データ一つ分のサイズ * 頂点データの要素数
 	UINT sizeVB = static_cast<UINT>(sizeof(XMFLOAT3) * _countof(vertices));
