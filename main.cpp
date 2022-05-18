a2098a8c14df01fdadd4210deb1b4a68c5362a8f
ShiTokuon <k021g1160@g.neec.ac.jp>
2022-05-18 09:42:50 +0900

    点・線・三角形

--- main.cpp
+++ main.cpp
@@ -240,6 +240,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		{ -0.5f, -0.5f, 0.0f }, // 左下
 		{ -0.5f, +0.5f, 0.0f }, // 左上
 		{ +0.5f, -0.5f, 0.0f }, // 右下
+
+		{ +0.5f, -0.5f, 0.0f }, // 右下
+		{ -0.5f, +0.5f, 0.0f }, // 左上
+		{ +0.5f, +0.5f, 0.0f }, // 右上
 	};
 
 	// 定数バッファ用データ構造体（マテリアル）
@@ -554,7 +558,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 // ビューポート設定コマンド
 
 		D3D12_VIEWPORT viewport{};
-		viewport.Width = 200;
+		viewport.Width = window_width;
 		viewport.Height = 200;
 		viewport.TopLeftX = 2;
 		viewport.TopLeftY = 8;
@@ -586,7 +590,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		commandList->SetGraphicsRootSignature(rootSignature);
 
 		// プリミティブ形状の設定コマンド
-		commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST); // 三角形リスト
+		commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP); // 三角形リスト
 
 // 頂点バッファビューの設定コマンド
 		commandList->IASetVertexBuffers(0, 1, &vbView);
