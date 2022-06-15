dd54c755a4b0e7b5f1310fc4c324cd579dedef27
ShiTokuon <k021g1160@g.neec.ac.jp>
2022-06-15 09:08:55 +0900

    04_03確認課題2

--- BasicPS.hlsl
+++ BasicPS.hlsl
@@ -4,7 +4,6 @@ Texture2D<float4> tex : register(t0);  	// 0番スロットに設定されたテクスチャ
 SamplerState smp : register(s0);      	// 0番スロットに設定されたサンプラー
 
 float4 main(VSOutput input) : SV_TARGET{
-	//*cloroは後で変更すること
-	return float4(tex.Sample(smp, input.uv)) * color;
+	return float4(tex.Sample(smp, input.uv));
 }
 
