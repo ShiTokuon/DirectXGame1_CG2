4932044f0fd90af0b5e4c21924fd2faed3455c74
ShiTokuon <k021g1160@g.neec.ac.jp>
2022-06-20 13:44:42 +0900

    05_03確認課題

--- main.cpp
+++ main.cpp
@@ -258,10 +258,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// 頂点データ
 	Vertex vertices[] = {
 		// x      y     z       u     v
-		{{0.0f, 100.0f, 0.0f}, {0.0f, 1.0f}}, // 左下
-		{{0.0f, 0.0f, 0.0f}, {0.0f, 0.0f}}, // 左上
-		{{100.0f, 100.0f, 0.0f}, {1.0f, 1.0f}}, // 右下
-		{{100.0f, 0.0f, 0.0f}, {1.0f, 0.0f}}, // 右上
+		{{-50.0f, -50.0f, 50.0f}, {0.0f, 1.0f}}, // 左下
+		{{-50.0f, 50.0f, 50.0f}, {0.0f, 0.0f}}, // 左上
+		{{50.0f, -50.0f, 50.0f}, {1.0f, 1.0f}}, // 右下
+		{{50.0f, 50.0f, 50.0f}, {1.0f, 0.0f}}, // 右上
 	};
 
 	// インデックスデータ
@@ -279,6 +279,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	//定数バッファ用データ構造体(3D変換行列)
 	struct ConstBufferDataTransform {
 		XMMATRIX mat; //3D変換行列
+		XMMATRIX oldVer;
+		XMMATRIX newVer;
 	};
 
 	ID3D12Resource* constBuffTransform = nullptr;
@@ -314,13 +316,40 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 
 	//単位行列を代入
+	constMapTransform->oldVer = XMMatrixIdentity();
 	constMapTransform->mat = XMMatrixIdentity();
 
-	constMapTransform->mat.r[0].m128_f32[0] = 2.0f / 1280;
-	constMapTransform->mat.r[1].m128_f32[1] = -2.0f / 720;
+	float matNumX = 2.0f / window_width;
+	float matNumY = -2.0f / window_height;
 
-	constMapTransform->mat.r[3].m128_f32[0] = -1.0f;
-	constMapTransform->mat.r[3].m128_f32[1] = 1.0f;
+	//constMapTransform->mat.r[0].m128_f32[0] = 2.0f / 1280;
+	//constMapTransform->mat.r[1].m128_f32[1] = -2.0f / 720;
+	//
+	//constMapTransform->mat.r[3].m128_f32[0] = -1.0f;
+	//constMapTransform->mat.r[3].m128_f32[1] = 1.0f;
+
+	constMapTransform->mat = XMMatrixOrthographicOffCenterLH(
+		0.0f, matNumX,
+		0.0f, matNumY,
+		0.0f, 1.0f
+	);
+
+	//透視投影行列の計算
+	constMapTransform->mat = XMMatrixPerspectiveFovLH(
+		XMConvertToRadians(45.0f),
+		(float)window_width / window_height,
+		0.1f, 1000.0f
+	);
+
+	XMMATRIX matProjection =
+		XMMatrixPerspectiveFovLH(
+			XMConvertToRadians(45.0f),
+			(float)window_width / window_height,
+			0.1f, 1000.0f
+		);
+
+	// 定数バッファに転送
+	constMapTransform->mat = matProjection;
 
 	// ヒープ設定
 	D3D12_HEAP_PROPERTIES cbHeapProp{};
